bda6d1597dbe81fe0472aeb14376fc509239d2d0
Sakib Rahman Sumon <42873074+SakibSumon@users.noreply.github.com>
2025-08-13 19:54:29 +0100

    Update model.py
    quantity addded

--- backend/model.py
+++ backend/model.py
@@ -1,4 +1,4 @@
-# backend/model.py
+
 import pandas as pd
 from prophet import Prophet
 import os
@@ -7,7 +7,11 @@ def load_data():
     # go one directory up to find dataset/
     file_path = os.path.join(os.path.dirname(__file__), "..", "dataset", "clothing_sales.csv")
     df = pd.read_csv(file_path, parse_dates=["order_date"])
-    df = df.groupby(["order_date", "sku"])["revenue"].sum().reset_index()
+    # df = df.groupby(["order_date", "sku"])["revenue"].sum().reset_index()
+    df = df.groupby(["order_date", "sku"]).agg({
+        "revenue": "sum",
+        "quantity": "sum"  # assuming the column is called "quantity"
+    }).reset_index()
     return df
 
 def forecast_sku(df, sku, target="revenue", days=30):
